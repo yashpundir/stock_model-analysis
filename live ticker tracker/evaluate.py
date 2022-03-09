cc0c4f447f53709f157f8df482b3602bc1ba0590
yash pundir <yashpundir7@gmail.com>
2022-03-09 18:25:41 +0530

    1st 30min candle & SL Hit concept update

--- live ticker tracker/evaluate.py
+++ live ticker tracker/evaluate.py
@@ -2,6 +2,7 @@ import datetime as dt
 from fyers_api import fyersModel
 import configparser
 import time
+import numpy as np
 
 # FYERS API AUTHENTICATION
 # Reading Configs
@@ -18,15 +19,83 @@ def get_active_data(df):
 def update_status(df):
     for i, row in df.iterrows():
         if dt.date.today() == dt.datetime.strptime(row['Expiry'], '%d%b%Y').date():
-            if row['Status'] == 'T1 Hit':
-                df.loc[i, 'Status'] = 'Expired-T1 Hit'
-            elif row['Status'] == 'SL Hit':
-                df.loc[i, 'Status'] = 'Expired-SL Hit'
+            if row['Status'] in ['T1 Hit', 'SL Hit']:
+                df.loc[i, 'Status'] = f"Expired-{row['Status']}"
             else:
-                df.loc[i, 'Status'] = 'Expired-Stagnant'
+                df.loc[i, 'Status'] = f"Expired-Stagnant"
+
+def fetch_candles(row, today):
+    now = dt.datetime.today()
+    if now.time() < dt.time(4, 20):
+        reso = 30
+    else:
+        reso = 15
+
+    hs_data = {"symbol": f"NSE:{row['Stock']}-EQ", "resolution":reso, "date_format":"1", "range_from":today, "range_to":today, "cont_flag":"1"}
+    hs1_data = {"symbol": f"NSE:{row['Ticker Name OPT']}", "resolution":reso, "date_format":"1", "range_from":today, "range_to":today, "cont_flag":"1"}
+    hs2_data = {"symbol": f"NSE:{row['Ticker Name FUT']}", "resolution":reso, "date_format":"1", "range_from":today, "range_to":today, "cont_flag":"1"}
+    hs = fyers.history(hs_data)
+    time.sleep(0.25)
+    hs1 = fyers.history(hs1_data)
+    time.sleep(0.25)
+    hs2 = fyers.history(hs2_data)
+
+    # Calculate the required epoch/candle
+    mins = (now.minute - (now.minute % 15) - reso) % 60
+    hr = now.hour if mins < now.minute else now.hour - 1
+    epoch = dt.datetime(now.year, now.month, now.day, hr, mins).timestamp()
+
+    try:            # index the required candle
+        sres = hs[np.where(np.array(hs) == epoch)[0][0]]
+        s1res = hs1[np.where(np.array(hs1) == epoch)[0][0]]
+        s2res = hs2[np.where(np.array(hs2) == epoch)[0][0]]
+    except:
+        print(f"Candle index not found for trade ID{row['Trade ID']}. Check logs!")
+
+    return sres, s1res, s2res
+
+def check_bull_SL(row, sres, ind, df):
+    extended_SL = row['SL'] - row['SL']*0.01                               # in 1% range of SL
+
+    if (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and row['Status']=='ACTIVE':     # UTC time zone
+        if sres[4] <= extended_SL:                      # [4] => close
+            df.loc[ind, 'Status'] = 'SL Hit'
+
+        elif extended_SL <= sres[4] <= row['SL']:
+            df.loc[ind, 'Status'] = 'SL Zone'
+
+    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and row['Status']=='SL Zone':
+        if sres[4] <= row['SL']:
+            df.loc[ind, 'Status'] = 'SL Hit'
+        else:
+            df.loc[ind, 'Status'] = 'ACTIVE'
+
+    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(3, 45)) and row['Status']=='SL Zone':
+        if sres[4] <= extended_SL:
+            df.loc[ind, 'Status'] = 'SL Hit'
+
+def check_bear_SL(row, sres, ind, df):
+    extended_SL = row['SL'] + row['SL']*0.01
+
+    if (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and row['Status']=='ACTIVE':     # UTC time zone
+        if sres[4] >= extended_SL:                      # [4] => close
+            df.loc[ind, 'Status'] = 'SL Hit'
+
+        elif row['SL'] <= sres[4] <= extended_SL:
+            df.loc[ind, 'Status'] = 'SL Zone'
+
+    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and row['Status']=='SL Zone':
+        if sres[4] >= row['SL']:
+            df.loc[ind, 'Status'] = 'SL Hit'
+        else:
+            df.loc[ind, 'Status'] = 'ACTIVE'
+
+    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(3, 45)) and row['Status']=='SL Zone':
+        if sres[4] >= extended_SL:
+            df.loc[ind, 'Status'] = 'SL Hit'
 
 #--------------------------------------------------#
-# FOR STRATEGY 1                                   #
+# STRATEGY 1                                       #
 #--------------------------------------------------#
 # updates data inplace (not good coding practice)
 
@@ -34,7 +103,7 @@ def master(df):
     today = dt.datetime.strftime(dt.datetime.today(), format="%Y-%m-%d")
     
     for ind, row in df.iterrows():
-        if row["Status"]!='Expired':
+        if row["Status"][:7]!='Expired':
         
             if row['Type']=='Bullish':
                 get_bull(df, ind, row, today)
@@ -46,26 +115,8 @@ def master(df):
                 
 def get_bull(df, ind, row, today):
     # Get data 
-    hs_data = {"symbol": f"NSE:{row['Stock']}-EQ", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                              "cont_flag":"1"}
-    ho_data = {"symbol": f"NSE:{row['Ticker Name OPT']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hf_data = {"symbol": f"NSE:{row['Ticker Name FUT']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hs = fyers.history(hs_data)
-    time.sleep(0.25)
-    ho = fyers.history(ho_data)
-    time.sleep(0.25)
-    hf = fyers.history(hf_data)
+    sres, ores, fres = fetch_candles(row, today)
 
-    try:
-        if dt.time(4, 0) < dt.datetime.now().time() < dt.time(9, 55):
-            i = -2
-        else:
-            i = -1
-        sres = hs['candles'][i]
-        ores = ho['candles'][i]
-        fres = hf['candles'][i]
-    except:
-        print(f"api response not ok for Trade ID {row['Trade ID']}")
-    
     # update common columns
     df.loc[ind, 'Stock CMP'] = sres[4]
     df.loc[ind, 'Current Price OPT'] = ores[4]
@@ -79,20 +130,18 @@ def get_bull(df, ind, row, today):
         evaluate_active_bull(df, ind, row, sres, ores, fres)
         
 def evaluate_active_bull(df, ind, row, sres, ores, fres):
-    
     # check/update status
     if sres[2] >= row['T1']:                                                       # [2] => high
         df.loc[ind, 'Status'] = 'T1 Hit'                                           # [3] => low
         df.loc[ind, 'Exit Price OPT'] = ores[3]                                    
         df.loc[ind, 'Exit Price FUT'] = fres[2]                                    
 
-    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and (sres[4] <= row['SL'] + row['SL']*0.01): # UTC time zone
-        if row['Status'] == 'ACTIVE':                                       # [4] => close     in 1% range of SL
-            df.loc[ind, 'Status'] = 'SL Zone'                   
-        else:
-            df.loc[ind, 'Status'] = 'SL Hit'
+    else:                                                                         # check SL Hit or not
+        check_bull_SL(row, sres, ind, df)
+        if df.loc[ind, 'Status'] == 'SL Hit':
             df.loc[ind, 'Exit Price OPT'] = ores[2]
-            df.loc[ind, 'Exit Price FUT'] = fres[3]                                     
+            df.loc[ind, 'Exit Price FUT'] = fres[3]
+
     
     # Calculate P&L
     if df.loc[ind, 'Status']=='ACTIVE' or df.loc[ind, 'Status']=='SL Zone':       # If T1/SL is still not hit
@@ -107,25 +156,7 @@ def evaluate_active_bull(df, ind, row, sres, ores, fres):
     
 def get_bear(df, ind, row, today):
     # Get data 
-    hs_data = {"symbol": f"NSE:{row['Stock']}-EQ", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                              "cont_flag":"1"}
-    ho_data = {"symbol": f"NSE:{row['Ticker Name OPT']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hf_data = {"symbol": f"NSE:{row['Ticker Name FUT']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hs = fyers.history(hs_data)
-    time.sleep(0.25)
-    ho = fyers.history(ho_data)
-    time.sleep(0.25)
-    hf = fyers.history(hf_data)
-
-    try:
-        if dt.time(4, 0) < dt.datetime.now().time() < dt.time(9, 55):
-            i = -2
-        else:
-            i = -1
-        sres = hs['candles'][i]
-        ores = ho['candles'][i]
-        fres = hf['candles'][i]
-    except:
-        print(f"api response not ok for Trade ID {row['Trade ID']}")
+    sres, ores, fres = fetch_candles(row, today)
     
     # update common columns
     df.loc[ind, 'Stock CMP'] = sres[4] 
@@ -140,18 +171,15 @@ def get_bear(df, ind, row, today):
         evaluate_active_bear(df, ind, row, sres, ores, fres)
         
 def evaluate_active_bear(df, ind, row, sres, ores, fres):
-    
     # check/update status
     if sres[3] <= row['T1']:                                       # [3] => low 
         df.loc[ind, 'Status'] = 'T1 Hit'                           # [2] => high
         df.loc[ind, 'Exit Price OPT'] = ores[3]                    
         df.loc[ind, 'Exit Price FUT'] = fres[3]                    
 
-    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and (sres[4] >= row['SL'] - row['SL']*0.01):    # UTC time zone
-        if row['Status'] == 'ACTIVE':                                       # [4] => close    in 1% range of SL
-            df.loc[ind, 'Status'] = 'SL Zone'                   
-        else:
-            df.loc[ind, 'Status'] = 'SL Hit'
+    else:
+        check_bear_SL(row, sres, ind, df)
+        if df.loc[ind, 'Status'] == 'SL Hit':
             df.loc[ind, 'Exit Price OPT'] = ores[2]
             df.loc[ind, 'Exit Price FUT'] = fres[2]                     
     
@@ -174,7 +202,7 @@ def Master(df):
     today = dt.datetime.strftime(dt.datetime.today(), format="%Y-%m-%d")
     
     for ind, row in df.iterrows():
-        if row["Status"]!='Expired':
+        if row["Status"][:7]!='Expired':
         
             if row['Type']=='Bullish':
                 Get_bull(df, ind, row, today)
@@ -184,25 +212,7 @@ def Master(df):
                 
 def Get_bull(df, ind, row, today):
     # Get data 
-    hs_data = {"symbol": f"NSE:{row['Stock']}-EQ", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                              "cont_flag":"1"}
-    ho_data = {"symbol": f"NSE:{row['Ticker Name OPT1']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hf_data = {"symbol": f"NSE:{row['Ticker Name OPT2']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hs = fyers.history(hs_data)
-    time.sleep(0.25)
-    ho = fyers.history(ho_data)
-    time.sleep(0.25)
-    hf = fyers.history(hf_data)
-
-    try:
-        if dt.time(4, 0) < dt.datetime.now().time() < dt.time(9, 55):
-            i = -2
-        else:
-            i = -1
-        sres = hs['candles'][i]
-        ores = ho['candles'][i]
-        o2res = hf['candles'][i]
-    except:
-        print(f"api response not ok for Trade ID {row['Trade ID']}")
+    sres, ores, o2res = fetch_candles(row, today)
     
     # update common columns
     df.loc[ind, 'Stock CMP'] = sres[4]
@@ -217,18 +227,15 @@ def Get_bull(df, ind, row, today):
         Evaluate_active_bull(df, ind, row, sres, ores, o2res)
         
 def Evaluate_active_bull(df, ind, row, sres, ores, o2res):
-    
     # check/update status
     if sres[2] >= row['T1']:                                                       # [2] => high
         df.loc[ind, 'Status'] = 'T1 Hit'                                           # [3] => low
         df.loc[ind, 'Exit Price OPT1'] = ores[2]                                    
         df.loc[ind, 'Exit Price OPT2'] = o2res[2]                                    
 
-    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and (sres[4] <= row['SL'] + row['SL']*0.01):  # UTC time zone
-        if row['Status'] == 'ACTIVE':                                       # [4] => close       in 1% range of SL
-            df.loc[ind, 'Status'] = 'SL Zone'                   
-        else:
-            df.loc[ind, 'Status'] = 'SL Hit'
+    else:
+        check_bull_SL(row, sres, ind, df)
+        if df.loc[ind, 'Status'] == 'SL Hit':
             df.loc[ind, 'Exit Price OPT1'] = ores[3]
             df.loc[ind, 'Exit Price OPT2'] = o2res[3]                                    
     
@@ -245,25 +252,7 @@ def Evaluate_active_bull(df, ind, row, sres, ores, o2res):
         
 def Get_bear(df, ind, row, today):
     # Get data 
-    hs_data = {"symbol": f"NSE:{row['Stock']}-EQ", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                              "cont_flag":"1"}
-    ho_data = {"symbol": f"NSE:{row['Ticker Name OPT1']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hf_data = {"symbol": f"NSE:{row['Ticker Name OPT2']}", "resolution":"15", "date_format":"1", "range_from":today, "range_to":today,                      "cont_flag":"1"}
-    hs = fyers.history(hs_data)
-    time.sleep(0.25)
-    ho = fyers.history(ho_data)
-    time.sleep(0.25)
-    hf = fyers.history(hf_data)
-
-    try:
-        if dt.time(4, 0) < dt.datetime.now().time() < dt.time(9, 55):
-            i = -2
-        else:
-            i = -1
-        sres = hs['candles'][i]
-        ores = ho['candles'][i]
-        o2res = hf['candles'][i]
-    except:
-        print(f"api response not ok for Trade ID {row['Trade ID']}")
+    sres, ores, o2res = fetch_candles(row, today)
     
     # update common columns
     df.loc[ind, 'Stock CMP'] = sres[4] 
@@ -285,11 +274,9 @@ def Evaluate_active_bear(df, ind, row, sres, ores, o2res):
         df.loc[ind, 'Exit Price OPT1'] = ores[2]                     
         df.loc[ind, 'Exit Price OPT2'] = o2res[2]                    
 
-    elif (dt.datetime.fromtimestamp(sres[0]).time() == dt.time(9, 45)) and (sres[4] >= row['SL'] - row['SL']*0.01): # UTC time zone
-        if row['Status'] == 'ACTIVE':                                       # [4] => close
-            df.loc[ind, 'Status'] = 'SL Zone'                   
-        else:
-            df.loc[ind, 'Status'] = 'SL Hit'
+    else:
+        check_bear_SL(row, sres, ind, df)
+        if df.loc[ind, 'Status'] == 'SL Hit':
             df.loc[ind, 'Exit Price OPT1'] = ores[3]
             df.loc[ind, 'Exit Price OPT2'] = o2res[3]                     
     
@@ -304,10 +291,9 @@ def Evaluate_active_bear(df, ind, row, sres, ores, o2res):
 
     df.loc[ind, 'Total P&L'] = df.loc[ind, 'P&L OPT1'] + df.loc[ind, 'P&L OPT2']        
         
-    
 # Push updated data back to google sheets
-def push_changes(sheet, df):
-    values = df.values.tolist()
+def push_changes(sheet, data):
+    values = data.values.tolist()
     for value in values:
         cell = sheet.find(str(value[0]), in_column=0)
         a1 = cell.address
